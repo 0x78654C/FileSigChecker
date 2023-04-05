328b6e274d6f45d853eb38fb4a84ff611b946f7d
mkbmain <michael.bourke@live.co.uk>
2023-04-05 09:29:07 +0100

    minor cleanUp

--- src/FileSigChecker.cs
+++ src/FileSigChecker.cs
@@ -2,27 +2,22 @@
  * Author: x_Coding
  * Description: File signature checker.
  */
- 
-using System;
-using System.IO;
-using System.Linq;
+
 using System.Text;
 
-
-namespace FileType
+namespace FileSigChecker
 {
     class FileTypeChecker
     {
-        private const string s_extFileName = "ext_list.txt";
-        private static string s_helpMessage { get; set; }
-        private static bool s_isExtFound= false;
-        private static bool s_isIterated= false;
-        
-        
+        private const string ExtFileName = "ext_list.txt";
+        private static string HelpMessage { get; set; }
+        private static bool _isIterated = false;
+
+
         // Main method.
         static void Main(string[] args)
         {
-            s_helpMessage = @" Usage of file extension tool:
+            HelpMessage = @" Usage of file extension tool:
  	FileSigChecker.exe <file_path>      : Display file path, extension, hex signature, and signature description.
  	FileSigChecker.exe <file_path> -ext : Display extension only.
  	FileSigChecker.exe -h               : Display this help message.
@@ -30,50 +25,50 @@ namespace FileType
 Version: 1.0.1
 Author: x_Coding (xcoding.dev@gmail.com)
  	";
-            if (!CheckExtFile(s_extFileName))
+            if (!CheckExtFile(ExtFileName))
             {
                 ColorConsoleWriteLine(ConsoleColor.Red,
                     "Error: File ext_list.txt is not present with file type signature. File must be located with the FileSigChecker executable!");
                 return;
             }
 
-            var extOnly = string.Empty;
-            var fileParam = string.Empty;
-
-            try
+            if (args.Length < 1)
             {
-                fileParam = args[0];
+                Console.WriteLine("At least one param is required");
+                return;
+            }
+
+            var extOnly = string.Empty;
+            var fileParam = args[0];
+            if (args.Length > 1)
+            {
                 extOnly = args[1];
             }
-            catch (Exception e)
-            {
-                // Ignore.
-            }
-            
+
             if (fileParam.Contains("-h"))
             {
-                Console.WriteLine(s_helpMessage);
+                Console.WriteLine(HelpMessage);
                 return;
             }
-            
-			if(!File.Exists(fileParam))
-			{
-			    ColorConsoleWriteLine(ConsoleColor.Red,
+
+            if (!File.Exists(fileParam))
+            {
+                ColorConsoleWriteLine(ConsoleColor.Red,
                     $"Error: File '{fileParam}' does not exist!");
                 return;
-			}
-			
+            }
+
 
             switch (extOnly)
             {
                 case "-ext":
-                    CheckFileSignature(s_extFileName, fileParam, true);
+                    CheckFileSignature(ExtFileName, fileParam, true);
                     return;
                 case "":
-                    CheckFileSignature(s_extFileName, fileParam, false);
+                    CheckFileSignature(ExtFileName, fileParam, false);
                     return;
                 default:
-                    CheckFileSignature(s_extFileName, fileParam, false);
+                    CheckFileSignature(ExtFileName, fileParam, false);
                     return;
             }
         }
@@ -85,16 +80,15 @@ Author: x_Coding (xcoding.dev@gmail.com)
         /// <returns>void</returns>
         private static bool CheckExtFile(string extFileName) => File.Exists(extFileName);
 
-
         /// <summary>
         /// Change color of a specific text in console.
         /// </summary>
         /// <param name="color">Console color</param>
         /// <param name="text"></param>
         /// <returns>void</returns>
-        public static void ColorConsoleWriteLine(ConsoleColor color, object data)
+        private static void ColorConsoleWriteLine(ConsoleColor color, object data)
         {
-            ConsoleColor currentForeground = Console.ForegroundColor;
+            var currentForeground = Console.ForegroundColor;
             Console.ForegroundColor = color;
             Console.WriteLine(data);
             Console.ForegroundColor = currentForeground;
@@ -110,49 +104,39 @@ Author: x_Coding (xcoding.dev@gmail.com)
         /// <returns>void</returns>
         private static void CheckFileSignature(string extFileName, string filePath, bool extOnly)
         {
-        
-        	string outMessage = "Unknown signature.";
+            var outMessage = "Unknown signature.";
             var extLines = File.ReadAllLines(extFileName);
-            string hexFile = HexDump.GetHex(filePath);
+            var hexFile = HexDump.GetHex(filePath);
             var fileInfo = new FileInfo(filePath);
-            var nameExt = fileInfo.Extension.Replace(".","");
-           
-            foreach (var line in extLines)
+            var nameExt = fileInfo.Extension.Replace(".", "");
+            var isExtFound = false;
+            foreach (var line in extLines.Where(line => !string.IsNullOrEmpty(line)))
             {
-                if (string.IsNullOrEmpty(line))
-                	continue;
-                	
                 var hex = line.Split('|')[0];
-                bool check = false;
-				check  = hexFile.Contains(hex); 
-				var ext = line.Split('|')[1];
+                var check = hexFile.Contains(hex);
+                var ext = line.Split('|')[1];
                 var description = line.Split('|')[2];
-                bool checkExt = s_isIterated? true : ext.Contains(nameExt);
-                
-                if (check && checkExt)
-                {
-                	s_isExtFound = true;
-                    if (extOnly)
-                        outMessage = ext;
-                    else
-                        outMessage = $@"
+
+                if (!check || (!_isIterated && !ext.Contains(nameExt))) continue;
+                isExtFound = true;
+                outMessage = extOnly
+                    ? ext
+                    : $@"
 -------------------------------------------------------------	
 File:          {filePath}
 Extension(s):  {ext}
 Hex signature: {hex}
 Description:   {description}
 -------------------------------------------------------------";
-                    break;
-                }
+                break;
             }
-            
-            if(s_isExtFound)
-            	Console.WriteLine(outMessage);
-            
-            if(!s_isExtFound && !s_isIterated)
+
+            if (isExtFound)
+                Console.WriteLine(outMessage);
+            else if (!_isIterated)
             {
-            	s_isIterated= true;
-            	CheckFileSignature(extFileName, filePath, extOnly);
+                _isIterated = true;
+                CheckFileSignature(extFileName, filePath, extOnly);
             }
         }
     }
@@ -166,17 +150,17 @@ Description:   {description}
         /// <returns>string</returns>
         public static string GetHex(string fileName)
         {
-            string outHex = "";
+            var outHex = "";
             try
             {
                 if (File.Exists(fileName))
                 {
                     using (var stream = File.OpenRead(fileName))
                     {
-                        int size = stream.Length < 50 ? (int)stream.Length : 50;
-                        byte[] buffer = new byte[size];
+                        var size = stream.Length < 50 ? (int) stream.Length : 50;
+                        var buffer = new byte[size];
                         var read = stream.Read(buffer, 0, size);
-                        outHex = Hex(buffer, 50);
+                        outHex = Hex(buffer, size);
                     }
                 }
             }
@@ -188,6 +172,8 @@ Description:   {description}
             return outHex;
         }
 
+        private static char[] HexChars { get; } = "0123456789ABCDEF".ToCharArray();
+
         /// <summary>
         /// Hex dump
         /// </summary>
@@ -196,31 +182,27 @@ Description:   {description}
         /// <returns>string</returns>
         private static string Hex(byte[] bytes, int bytesPerLine = 50)
         {
-            if (bytes == null) return "<null>";
-            int bytesLength = bytes.Length;
+            if (bytes is null) return "<null>";
+            var bytesLength = bytes.Length;
 
-            char[] HexChars = "0123456789ABCDEF".ToCharArray();
-
-
-            int firstCharColumn = 0
+            var firstCharColumn = 0
                                   + bytesPerLine * 3
                                   + (bytesPerLine - 1) / 8;
 
 
-            int lineLength = firstCharColumn
+            var lineLength = firstCharColumn
                              + bytesPerLine
                              + Environment.NewLine.Length;
 
-            char[] line = (new String(' ', lineLength - Environment.NewLine.Length) + Environment.NewLine).ToCharArray();
-            int expectedLines = (bytesLength + bytesPerLine - 1) / bytesPerLine;
-            StringBuilder result = new StringBuilder(expectedLines * lineLength);
+            var line = (new string(' ', lineLength - Environment.NewLine.Length) + Environment.NewLine).ToCharArray();
+            var expectedLines = (bytesLength + bytesPerLine - 1) / bytesPerLine;
+            var result = new StringBuilder(expectedLines * lineLength);
 
-            for (int i = 0; i < bytesLength; i += bytesPerLine)
+            for (var i = 0; i < bytesLength; i += bytesPerLine)
             {
-                int hexColumn = 0;
-                int charColumn = firstCharColumn;
+                var hexColumn = 0;
 
-                for (int j = 0; j < bytesPerLine; j++)
+                for (var j = 0; j < bytesPerLine; j++)
                 {
                     if (i + j >= bytesLength)
                     {
@@ -229,7 +211,7 @@ Description:   {description}
                     }
                     else
                     {
-                        byte b = bytes[i + j];
+                        var b = bytes[i + j];
                         line[hexColumn] = HexChars[(b >> 4) & 0xF];
                         line[hexColumn + 1] = HexChars[b & 0xF];
                     }
